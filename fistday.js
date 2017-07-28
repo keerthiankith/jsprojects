ff60adc4e6c86c5963f94dfef200e5b2e19b81be
keerthiankith <ankiwanaburs@gmail.com>
2017-07-28 18:04:47 +0530

    add comtent

---
+++ fistday.js
@@ -0,0 +1,14 @@
+/*var a=10;
+console.log(typeof(a));*/
+/*var a=10;
+console.log(a);
+var a=20;
+console.log(a);*/
+var a=10;
+function sum(){
+	a=20;
+	console.log(a);
+}
+sum();
+console.log(a);
+
